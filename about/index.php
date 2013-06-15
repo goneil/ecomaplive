f3b47d7d965071ec4c1acd8c9dbd567ad098fb10
Gil O'Neil <goneil@mit.edu>
2013-06-15 17:22:05 -0400

    added bootstrap to more pages

--- about/index.php
+++ about/index.php
@@ -5,17 +5,22 @@
 <head>
     <link rel="stylesheet" type="text/css" href="<?php echo
     $_SERVER['SERVER_HOST'];?>/images/css/ecostyle.css" />
+    <link rel="stylesheet" type="text/css" href="http://<?php echo
+    $_SERVER['HTTP_HOST'];?>/images/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="http://<?php echo
+    $_SERVER['HTTP_HOST'];?>/images/css/bootstrap-responsive.min.css" />
 </head>
 <body>
 <div class="all">
     <div class="banner" id="banner1"><img src="<?php echo $_SERVER['SERVER_HOST'];?>/images/grassbanner1.jpg"></img></div>
     <?php include ("../mainmenu.php"); ?>
     <div class="content">
+      <h2>About</h2>
       <p>
       <ul>
-	<li><a href="team.php">Our Team</a></li>
-	<li><a href="vision.php">Vision</a></li>
-	<li><a href="contact.php">Contact</a></li>
+        <li><a href="team.php">Our Team</a></li>
+        <li><a href="vision.php">Vision</a></li>
+        <li><a href="contact.php">Contact</a></li>
       </ul>
       </p>
     </div>
--- tech/index.php
+++ tech/index.php
@@ -9,7 +9,6 @@
     $_SERVER['HTTP_HOST'];?>/images/css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="http://<?php echo
     $_SERVER['HTTP_HOST'];?>/images/css/bootstrap-responsive.min.css" />
-
 </head>
 <body>
 <div class="all">
