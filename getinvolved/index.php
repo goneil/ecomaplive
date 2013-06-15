a2db0650f617a8d7dc629146eecbf5feb8a0189b
Gil O'Neil <goneil@mit.edu>
2013-06-15 17:19:00 -0400

    added bootstrap to all pages

--- getinvolved/index.php
+++ getinvolved/index.php
@@ -5,18 +5,23 @@
 <head>
     <link rel="stylesheet" type="text/css" href="<?php echo
     $_SERVER['SERVER_HOST'];?>/images/css/ecostyle.css" />
+    <link rel="stylesheet" type="text/css" href="http://<?php echo
+    $_SERVER['HTTP_HOST'];?>/images/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="http://<?php echo
+    $_SERVER['HTTP_HOST'];?>/images/css/bootstrap-responsive.min.css" />
 </head>
 <body>
 <div class="all">
     <div class="banner" id="banner4"><img src="<?php echo $_SERVER['SERVER_HOST'];?>/getinvolved/bannergetinvolved.jpg"></img></div>
     <?php include ("../mainmenu.php"); ?>
     <div class="content">
+      <h2>Get Involved</h2>
       <p>
       <ul>
-	<li><a href="students.php">Students</a></li>
-	<li><a href="teachers.php">Teachers</a></li>
-	<li><a href="ngos.php">NGOs</a></li>
-	<li><a href="developers.php">Developers</a></li>
+        <li><a href="students.php">Students</a></li>
+        <li><a href="teachers.php">Teachers</a></li>
+        <li><a href="ngos.php">NGOs</a></li>
+        <li><a href="developers.php">Developers</a></li>
       </ul>
       </p>
     </div>
--- tech/index.php
+++ tech/index.php
@@ -5,12 +5,18 @@
 <head>
     <link rel="stylesheet" type="text/css" href="<?php echo
     $_SERVER['SERVER_HOST'];?>/images/css/ecostyle.css" />
+    <link rel="stylesheet" type="text/css" href="http://<?php echo
+    $_SERVER['HTTP_HOST'];?>/images/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="http://<?php echo
+    $_SERVER['HTTP_HOST'];?>/images/css/bootstrap-responsive.min.css" />
+
 </head>
 <body>
 <div class="all">
     <div class="banner" id="banner2"><img src="<?php echo $_SERVER['SERVER_HOST'];?>/tech/bannertech.jpg"></img></div>
     <?php include ("../mainmenu.php"); ?>
     <div class="content">
+      <h2>Technology</h2>
       <p>
       <ul>
        <li><a href="/tech/intro.php">Intro</a></li>
